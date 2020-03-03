b2a876a4b35b28f5c3651cdd0b90b27c02aef319
Constantin Nickel <constantin.nickel@gmail.com>
2020-03-03 14:30:57 +0100

    Add some filters for games and mods

---
+++ Filters/GameFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace Modio.Filters
+{
+    public class GameFilter
+    {
+        public static readonly FullTextField FullText = new FullTextField();
+        public static readonly NumericField<uint> Id = new NumericField<uint>("id");
+        public static readonly TextField Name = new TextField("name");
+        public static readonly TextField NameId = new TextField("name_id");
+    }
+}
