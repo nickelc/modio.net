e7796d47fe5f6b4ffcda914066951d3ce902d184
Constantin Nickel <constantin.nickel@gmail.com>
2020-03-10 14:06:39 +0100

    Add Client constructor for custom base url

--- Client.cs
+++ Client.cs
@@ -5,6 +5,7 @@ namespace Modio
     public class Client
     {
         public static readonly Uri ModioApiUrl = new Uri("https://api.mod.io/v1/");
+        public static readonly Uri ModioApiTestUrl = new Uri("https://api.test.mod.io/v1/");
 
         private IConnection connection;
 
@@ -14,10 +15,29 @@ namespace Modio
         {
         }
 
+        public Client(Uri baseUrl, Credentials credentials) : this(new Connection(FixBaseUrl(baseUrl), credentials))
+        {
+        }
+
         private Client(IConnection connection)
         {
             this.connection = connection;
             Games = new GamesClient(connection);
+        }
+
+        static Uri FixBaseUrl(Uri uri)
+        {
+            Ensure.ArgumentNotNull(uri, nameof(uri));
+            if (uri.Host.Equals("api.mod.io"))
+            {
+                return ModioApiUrl;
+            }
+            if (uri.Host.Equals("api.test.mod.io"))
+            {
+                return ModioApiTestUrl;
+            }
+
+            return new Uri(uri, new Uri("/v1/", UriKind.Relative));
         }
     }
 }
