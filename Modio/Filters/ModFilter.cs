b63d348ceed368563a34486f8b20e95e9464709b
Constantin Nickel <constantin.nickel@gmail.com>
2022-03-15 19:18:21 +0100

    Add missing search fields to `ModFilter`

--- Modio/Filters/ModFilter.cs
+++ Modio/Filters/ModFilter.cs
@@ -16,6 +16,46 @@
         public static readonly NumericField<uint> Id = new NumericField<uint>("id");
 
         /// <summary>
+        /// Filter for game id.
+        /// </summary>
+        public static readonly NumericField<uint> GameId = new NumericField<uint>("game_id");
+
+        /// <summary>
+        /// Filter for status.
+        /// </summary>
+        public static readonly NumericField<uint> Status = new NumericField<uint>("status");
+
+        /// <summary>
+        /// Filter for visble.
+        /// </summary>
+        public static readonly NumericField<uint> Visible = new NumericField<uint>("visible");
+
+        /// <summary>
+        /// Filter for submitted_by.
+        /// </summary>
+        public static readonly NumericField<uint> SubmitterId = new NumericField<uint>("submitted_by");
+
+        /// <summary>
+        /// Filter for date_added.
+        /// </summary>
+        public static readonly NumericField<uint> DateAdded = new NumericField<uint>("date_added");
+
+        /// <summary>
+        /// Filter for date_updated.
+        /// </summary>
+        public static readonly NumericField<uint> DateUpdated = new NumericField<uint>("date_updated");
+
+        /// <summary>
+        /// Filter for date_live.
+        /// </summary>
+        public static readonly NumericField<uint> DateLive = new NumericField<uint>("date_live");
+
+        /// <summary>
+        /// Filter for maturity_option.
+        /// </summary>
+        public static readonly NumericField<uint> MaturityOption = new NumericField<uint>("maturity_option");
+
+        /// <summary>
         /// Filter for name.
         /// </summary>
         public static readonly TextField Name = new TextField("name");
@@ -24,6 +64,41 @@
         /// Filter for name_id.
         /// </summary>
         public static readonly TextField NameId = new TextField("name_id");
+
+        /// <summary>
+        /// Filter for summary.
+        /// </summary>
+        public static readonly TextField Summary = new TextField("summary");
+
+        /// <summary>
+        /// Filter for description.
+        /// </summary>
+        public static readonly TextField Description = new TextField("description");
+
+        /// <summary>
+        /// Filter for homepage_url.
+        /// </summary>
+        public static readonly TextField HomepageUrl = new TextField("homepage_url");
+
+        /// <summary>
+        /// Filter for modfile.
+        /// </summary>
+        public static readonly NumericField<uint> ModfileId = new NumericField<uint>("modfile");
+
+        /// <summary>
+        /// Filter for metadata_blob.
+        /// </summary>
+        public static readonly TextField MetadataBlob = new TextField("metadata_blob");
+
+        /// <summary>
+        /// Filter for metadata_kvp.
+        /// </summary>
+        public static readonly TextField MetadataKeyValue = new TextField("metadata_kvp");
+
+        /// <summary>
+        /// Filter for tags.
+        /// </summary>
+        public static readonly TextField Tags = new TextField("tags");
 
         /// <summary>
         /// Sort results by most downloads.
