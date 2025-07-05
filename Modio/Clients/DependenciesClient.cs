0595c657b0948ebab5ce68198525edbb73fede5a
Constantin Nickel <constantin.nickel@gmail.com>
2025-07-05 11:55:20 +0200

    Remove unnecessary `using` directives

--- Modio/Clients/DependenciesClient.cs
+++ Modio/Clients/DependenciesClient.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 using Modio.Models;
--- Modio/Http/Response.cs
+++ Modio/Http/Response.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
 
 namespace Modio
 {
