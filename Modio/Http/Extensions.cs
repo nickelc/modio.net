09d3a746f19e1275cedef99d611af4aecb2b38e9
Constantin Nickel <constantin.nickel@gmail.com>
2025-07-24 14:30:03 +0200

    Move related helpers into the Http folder

---
+++ Modio/Http/Extensions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace Modio;
+
+internal static class HttpContentExtensions
+{
+    public static StringContent ToContent(this string content)
+    {
+        return new StringContent(content);
+    }
+
+    public static StreamContent ToContent(this FileInfo fileInfo)
+    {
+        return new StreamContent(fileInfo.OpenRead());
+    }
+}
+
+internal static class UriExtensions
+{
+    public static Uri ApplyParameters(this Uri uri, IDictionary<string, string> parameters)
+    {
+        Ensure.ArgumentNotNull(uri, nameof(uri));
+
+        if (parameters == null || !parameters.Any()) return uri;
+
+        IDictionary<string, string> p = new Dictionary<string, string>(parameters);
+
+        var hasQueryString = uri.OriginalString.IndexOf("?", StringComparison.Ordinal);
+
+        string uriWithoutQuery = hasQueryString == -1
+                ? uri.ToString()
+                : uri.OriginalString.Substring(0, hasQueryString);
+
+        string queryString;
+        if (uri.IsAbsoluteUri)
+        {
+            queryString = uri.Query;
+        }
+        else
+        {
+            queryString = hasQueryString == -1
+                ? ""
+                : uri.OriginalString.Substring(hasQueryString);
+        }
+
+        var values = queryString.Replace("?", "")
+                                .Split(new[] { "&" }, StringSplitOptions.RemoveEmptyEntries);
+
+        var existingParameters = values.ToDictionary(
+                    key => key.Substring(0, key.IndexOf('=')),
+                    value => value.Substring(value.IndexOf('=') + 1));
+
+        foreach (var existing in existingParameters)
+        {
+            if (!p.ContainsKey(existing.Key))
+            {
+                p.Add(existing);
+            }
+        }
+
+        string query = string.Join("&", p.Select(kvp => kvp.Key + '=' + Uri.EscapeDataString(kvp.Value)));
+        if (uri.IsAbsoluteUri)
+        {
+            var uriBuilder = new UriBuilder(uri)
+            {
+                Query = query
+            };
+            return uriBuilder.Uri;
+        }
+
+        return new Uri(uriWithoutQuery + '?' + query, UriKind.Relative);
+    }
+}
